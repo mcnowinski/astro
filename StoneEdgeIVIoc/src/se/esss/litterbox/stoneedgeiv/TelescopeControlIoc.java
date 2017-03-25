df38828154f1294f0eca81eb7dc15645ac12d883
david-mcginnis <dmcginnis427@gmail.com>
2017-03-25 15:04:25 +0100

    Telescope status added to Ioc

--- StoneEdgeIVIoc/src/se/esss/litterbox/stoneedgeiv/TelescopeControlIoc.java
+++ StoneEdgeIVIoc/src/se/esss/litterbox/stoneedgeiv/TelescopeControlIoc.java
@@ -14,12 +14,50 @@ public class TelescopeControlIoc extends IceCubePeriodicPollIoc
 	{
 		super(clientId, mqttBrokerInfoFilePath);
 	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public byte[] getDataFromGizmo() 
 	{
+		try
+		{
+			String response;
+			response = this.runExternalProcess("tx slit", true, true)[0];
+			publishResponse(response, "tel/done/tx/slit");
+			response = this.runExternalProcess("tx track", true, true)[0];
+			publishResponse(response, "tel/done/tx/track");
+			response = this.runExternalProcess("tx dome", true, true)[0];
+			publishResponse(response, "tel/done/tx/dome");
+			response = this.runExternalProcess("tx lamps", true, true)[0];
+			publishResponse(response, "tel/done/tx/lamps");
+			response = this.runExternalProcess("tx where", true, true)[0];
+			publishResponse(response, "tel/done/tx/where");
+			response = this.runExternalProcess("tx lock", true, true)[0];
+			publishResponse(response, "tel/done/tx/lock");
+			response = this.runExternalProcess("tx focus", true, true)[0];
+			publishResponse(response, "tel/done/tx/focus");
+			response = this.runExternalProcess("lst", true, true)[0];
+			JSONObject outputData = new JSONObject();
+			outputData.put("lst", response);
+			response = this.runExternalProcess("date", true, true)[0];
+			outputData.put("local", response);
+			response = this.runExternalProcess("date -u", true, true)[0];
+			outputData.put("gmt", response);
+			publishMessage("tel/done/time", outputData.toJSONString().getBytes(), 0, true);
+			response = this.runExternalProcess("pfilter", true, true)[0];
+			outputData = new JSONObject();
+			outputData.put("filter", response);
+			publishMessage("tel/done/pfilter", outputData.toJSONString().getBytes(), 0, true);
+			response = this.runExternalProcess("tin", true, true)[0];
+			publishResponse(response, "tel/done/tin");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error: " + e.getMessage()); 
+			return null;
+
+		}
 		return null;
 	}
-	@SuppressWarnings("unchecked")
 	@Override
 	public void handleBrokerMqttMessage(String topic, byte[] message) 
 	{
@@ -49,25 +87,9 @@ public class TelescopeControlIoc extends IceCubePeriodicPollIoc
 				response = this.runExternalProcess(command, true, true)[0];
 			}
 			System.out.println(response);
-			String[] responseParsed = response.split(" ");
 
-			JSONObject outputData = new JSONObject();
+			publishResponse(response, "tel/done/" + topic.substring(topic.indexOf("/set/") + 5));
 
-			for (int ii = 0; ii < responseParsed.length; ++ii)
-			{
-				int equalPos = responseParsed[ii].indexOf("=");
-				String key = "";
-				String val = "";
-				if (equalPos > 0)
-				{
-					key = responseParsed[ii].substring(0, equalPos);
-					val = responseParsed[ii].substring(equalPos + 1);
-					outputData.put(key, val);
-					
-//					System.out.println(key + ":" + val);
-				}
-			}
-			publishMessage("tel/done/" + topic.substring(topic.indexOf("/set/") + 5), outputData.toJSONString().getBytes(), 0, true);
 		}
 		catch (Exception e) 
 		{
@@ -75,6 +97,29 @@ public class TelescopeControlIoc extends IceCubePeriodicPollIoc
 			return;
 		}
 				
+	}
+	@SuppressWarnings("unchecked")
+	private void publishResponse(String response, String topic) throws Exception
+	{
+		String[] responseParsed = response.split(" ");
+		JSONObject outputData = new JSONObject();
+
+		for (int ii = 0; ii < responseParsed.length; ++ii)
+		{
+			int equalPos = responseParsed[ii].indexOf("=");
+			String key = "";
+			String val = "";
+			if (equalPos > 0)
+			{
+				key = responseParsed[ii].substring(0, equalPos);
+				val = responseParsed[ii].substring(equalPos + 1);
+				outputData.put(key, val);
+				
+//				System.out.println(key + ":" + val);
+			}
+		}
+		publishMessage(topic, outputData.toJSONString().getBytes(), 0, true);
+		
 	}
 	@SuppressWarnings("rawtypes")
 	private String[][] getJsonArray(String topic, byte[] message) throws Exception  
@@ -107,7 +152,7 @@ public class TelescopeControlIoc extends IceCubePeriodicPollIoc
 	}
 	public static void main(String[] args) throws Exception 
 	{
-		int periodicPoll = 100;//Integer.parseInt(args[0]);
+		int periodicPoll = 1000;//Integer.parseInt(args[0]);
 		TelescopeControlIoc ioc = new TelescopeControlIoc("TelescopeControlIoc", "stoneEdgeMqttBroker.dat");
 		ioc.setPeriodicPollPeriodmillis(periodicPoll);
 		ioc.startIoc("tel/set/#", "tel/done");
